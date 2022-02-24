4a46753a7a7763f07ba880fab20b0bd712f74249
msalexandra <msaalexandra96@gmail.com>
2022-02-25 01:43:30 +0300

    Выполнила задание module2-task1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+function getRandomNumber(min, max) {
+  const random = min - 0.5 + Math.random() * (max - min + 1);
+  if (min===max || min > max || min < 0) {
+    return 'Введите другое значение';
+  }
+  return Math.round(random);
+}
 
+
+function getRandomCoordinat(min, max, decimalPlaces) {
+  if (min===max || min > max || min < 0) {
+    return ('Введите другое значение');
+  }
+  return ((Math.random() * (max - min) + min).toFixed(decimalPlaces));
+}
+
+
+getRandomNumber(0, 10);
+getRandomCoordinat(2.2, 2.1, 5);
