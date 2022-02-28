f42cd9d09a416b5e96a2826faddea51d29b4fdf6
msalexandra <msaalexandra96@gmail.com>
2022-02-28 19:31:53 +0300

    Выполнила задание module1-task1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 function getRandomNumber(min, max) {
   const random = min - 0.5 + Math.random() * (max - min + 1);
-  if (min===max || min > max || min < 0) {
-    return 'Введите другое значение';
+  if (min > max) {
+    throw new SyntaxError('Пожалуйста, поменяйте числа местами');
+  } if (min < 0) {
+    throw new SyntaxError('Пожалуйста, введите положительные значения');
   }
   return Math.round(random);
 }
 
 
 function getRandomCoordinat(min, max, decimalPlaces) {
-  if (min===max || min > max || min < 0) {
-    return ('Введите другое значение');
+  if (min > max) {
+    throw new SyntaxError('Пожалуйста, поменяйте числа местами');
+  } if (min < 0) {
+    throw new SyntaxError('Пожалуйста, введите положительные значения');
   }
   return ((Math.random() * (max - min) + min).toFixed(decimalPlaces));
 }
 
 
-getRandomNumber(0, 10);
-getRandomCoordinat(2.2, 2.1, 5);
+getRandomNumber(4, 10);
+getRandomCoordinat(-2.1, 2.4, 2);
